853e785e7dbbaa86f775df1d8b7ca0b7f52f9c88
rezafebran <73734858+rezafebran@users.noreply.github.com>
2021-11-11 21:37:25 +0700

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,26 @@
+<?php
+$telegram_id = $_POST['chatid'];
+$message_text = $_POST['pesan'];
+$secret_token = '2136716433:AAGJYfY2QeQfJVmMEPorKVWEVMBho7K9tdQ';
+sendMessage($telegram_id, $message_text, $secret_token);
+
+function sendMessage($telegram_id, $message_text, $secret_token) {
+    $url = "https://api.telegram.org/bot" . $secret_token . "/sendMessage?parse_mode=markdown&chat_id=" . $telegram_id=taro id disini; //ganti = id sama id lu klo pesan kirim ke pc pake get bot id search di telegram klo grup bisa ke https://dicoffeean.com/bot-telegram-laporan/
+    $url = $url . "&text=" . urlencode($message_text);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    $err = curl_error($ch);
+    curl_close($ch);
+
+    if ($err) {
+       echo 'Pesan gagal terkirim, error :' . $err;
+    }else{
+        echo 'Pesan terkirim';
+    }
+}
+?>
